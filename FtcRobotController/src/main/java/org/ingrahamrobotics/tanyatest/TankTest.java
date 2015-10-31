7626c83c8ddf30dc67e92555cc85334c2721c46d
Ariel <raarielgrace@gmail.com>
2015-10-30 17:36:47 -0700

    More gamepad debug

--- FtcRobotController/src/main/java/org/ingrahamrobotics/tanyatest/TankTest.java
+++ FtcRobotController/src/main/java/org/ingrahamrobotics/tanyatest/TankTest.java
@@ -13,46 +13,23 @@ public class TankTest extends OpMode {
     DcMotor motorLeft;
 
     public void init() {
-		/*
-		 * Use the hardwareMap to get the dc motors and servos by name. Note
-		 * that the names of the devices must match the names used when you
-		 * configured your robot and created the configuration file.
-		 */
-
-		/*
-		 * For the demo Tetrix K9 bot we assume the following,
-		 *   There are two motors "motor_1" and "motor_2"
-		 *   "motor_1" is on the right side of the bot.
-		 *   "motor_2" is on the left side of the bot.
-		 *
-		 * We also assume that there are two servos "servo_1" and "servo_6"
-		 *    "servo_1" controls the arm joint of the manipulator.
-		 *    "servo_6" controls the claw joint of the manipulator.
-		 */
         motorRight = hardwareMap.dcMotor.get("Right");
         motorLeft = hardwareMap.dcMotor.get("Left");
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
-
     }
 
     public void loop() {
 
-		/*
-		 * Gamepad 1
-		 *
-		 * Gamepad 1 controls the motors via the left stick, and it controls the
-		 * wrist/claw via the a,b, x, y buttons
-		 */
-
-        // tank drive
         // note that if y equal -1 then joystick is pushed all of the way forward.
         float left = -gamepad2.left_stick_y;
         float right = -gamepad2.right_stick_y;
 
-        telemetry.addData("G1LY", gamepad1.left_stick_x);
+        telemetry.addData("G1LY", gamepad1.left_stick_y);
         telemetry.addData("G1RY", gamepad1.right_stick_y);
         telemetry.addData("G2LY", gamepad2.left_stick_x);
         telemetry.addData("G2RY", gamepad2.right_stick_y);
+        telemetry.addData("G1X", gamepad1.x);
+        telemetry.addData("G2X", gamepad2.x);
 
         // clip the right/left values so that the values never exceed +/- 1
         right = Range.clip(right, -1, 1);
@@ -62,14 +39,8 @@ public class TankTest extends OpMode {
         motorRight.setPower(right);
         motorLeft.setPower(left);
 
-		/*
-		 * Send telemetry data back to driver station. Note that if we are using
-		 * a legacy NXT-compatible motor controller, then the getPower() method
-		 * will return a null value. The legacy NXT-compatible motor controllers
-		 * are currently write only.
-		 */
-        telemetry.addData("left tgt pwr",  "left  pwr: " + String.format("%.2f", left));
-        telemetry.addData("right tgt pwr", "right pwr: " + String.format("%.2f", right));
+        telemetry.addData("L Out", left);
+        telemetry.addData("R Out", right);
     }
 
 }
