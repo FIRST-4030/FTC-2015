b2225d74ff3d9b8d887b00d52e0ebe0444f203d4
DroppingDonuts <raarielgrace@gmail.com>
2015-11-15 17:09:42 -0800

    Added other systems to teleop.

--- FtcRobotController/src/main/java/org/ingrahamrobotics/ftc2015/TeleOp.java
+++ FtcRobotController/src/main/java/org/ingrahamrobotics/ftc2015/TeleOp.java
@@ -10,24 +10,47 @@ import com.qualcomm.robotcore.util.Range;
  */
 public class TeleOp extends OpMode {
 
+    //NOTE: THIS CODE IS MOSTLY UNTESTED AND USES HARDWARE THAT IS NOT YET ON THE ROBOT
+    //Follow the instructions in the comments on how to use the code
+    //Hook mechanics are not yet added, because I didn't know them
+
+
     DcMotor motorLeft;
     DcMotor motorRight;
+    //Even if they are not on the robot, having undefined fields will not matter in the code
+    DcMotor lift;
+    DcMotor collectorIntake;
     Servo collectorTilt;
     Servo zipLineLeft;
     Servo zipLineRight;
-    //The servos accept floats between 0 and 1
-    final int SERVO_DOWN = 0;
-    final int SERVO_UP = 1;
 
+    //The servos accept floats between 0 and 1
+    final float SERVO_DOWN = 0F;
+    final float SERVO_UP = 1F;
+    final int LIFT_UP = 100000;
+    final int LIFT_DOWN = 0;
+    //Test value for lift is full speed positive. can be changed if necessary
+    final double LIFT_SPEED = 1.0;
+    //Test values for collector are full speed opposite directions. can be changed
+    final double COLLECTOR_IN = 1.0;
+    final double COLLECTOR_OUT = 1.0;
 
     @Override
     public void init() {
         motorRight = hardwareMap.dcMotor.get("right_drive");
         motorLeft = hardwareMap.dcMotor.get("left_drive");
+        //These parts should be commented out based on whether or not they're on the robot
+        //The code won't work properly if the unadded parts are initialized
+        //lift = hardwareMap.dcMotor.get("lift");
+        //collectorIntake = hardwareMap.dcMotor.get("collector_intake");
         //collectorTilt = hardwareMap.servo.get("collector_tilt");
         zipLineLeft = hardwareMap.servo.get("zip_line_left");
         zipLineRight = hardwareMap.servo.get("zip_line_right");
+
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
+        //lift.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        //lift.setTargetPosition(LIFT_DOWN);
+        //lift.setPower(LIFT_SPEED);
     }
 
     @Override
@@ -52,7 +75,13 @@ public class TeleOp extends OpMode {
         motorRight.setPower(right);
         motorLeft.setPower(left);
 
+        //NOTE: CURRENTLY EVERYTHING IS MAPPED TO THE FIRST JOYSTICK FOR EASE OF TESTING
+        //Can be remapped after discussion with drivers to the final positions
+
         //will move the zipline servos up and down
+        /*in this case, SERVO_UP is used to describe when the flags are inside
+        of the robot and SERVO_DOWN refers to when they're sticking out to hit the zipline
+        */
         if(gamepad1.x) {
             zipLineLeft.setPosition(SERVO_UP);
         } else if(gamepad1.a) {
@@ -66,10 +95,30 @@ public class TeleOp extends OpMode {
         }
 
         //raises (tilts) and lowers the collector to score
+        /*in this case SERVO_DOWN is used to describe when the collector is flat
+        and
+        */
+        /*
         if(gamepad1.left_bumper) {
             collectorTilt.setPosition(SERVO_DOWN);
         } else if(gamepad1.right_bumper) {
             collectorTilt.setPosition(SERVO_UP);
         }
+
+        //sets the lifts to one preset high and lowers it
+        if(gamepad1.dpad_down) {
+            lift.setTargetPosition(LIFT_DOWN);
+        } else if(gamepad1.dpad_up) {
+            lift.setTargetPosition(LIFT_UP);
+        }
+
+        //sets the intake spinners going in and out
+        //may want to map this to a joystick on controller 2
+        if(gamepad1.left_stick_button) {
+            collectorIntake.setPower(COLLECTOR_OUT);
+        } else if(gamepad1.right_stick_button) {
+            collectorIntake.setPower(COLLECTOR_IN);
+        }
+        */
     }
 }
